c9e728b7e4b95638bd47a0c43249738fa8eafb4c
arcade <aashisharcade@gmail.com>
2025-01-08 23:53:58 +0530

    Create update_readme.py

---
+++ update_readme.py
@@ -0,0 +1,36 @@
+import json
+
+# File paths
+README_FILE = "README.md"
+BLOGS_JSON_FILE = "devto_blogs.json"
+
+# Load the latest blogs
+with open(BLOGS_JSON_FILE, "r") as file:
+    blogs = json.load(file)
+
+# Get the top 5 blogs
+latest_blogs = blogs[:5]
+
+# Generate the markdown list for blogs
+blogs_md = "\n".join(
+    [f"- [{blog['title']}]({blog['url']}) - {blog['published_at'][:10]}" for blog in latest_blogs]
+)
+
+# Update the README.md
+with open(README_FILE, "r+") as file:
+    content = file.read()
+    start_marker = "<!-- BLOGS:START -->"
+    end_marker = "<!-- BLOGS:END -->"
+    start_idx = content.find(start_marker)
+    end_idx = content.find(end_marker)
+
+    # Replace the blogs section
+    if start_idx != -1 and end_idx != -1:
+        new_content = (
+            content[:start_idx + len(start_marker)]
+            + "\n" + blogs_md + "\n"
+            + content[end_idx:]
+        )
+        file.seek(0)
+        file.write(new_content)
+        file.truncate()
